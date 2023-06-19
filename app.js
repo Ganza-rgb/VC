ddf64e076e794f4ae6109ad27ff27cd577f64969
Ganza Eliphaz <ganzaeliphaz@gmail.com>
2023-06-19 03:18:58 -0700

    adding new file

---
+++ app.js
@@ -0,0 +1,16 @@
+//JavaScript Decralation
+
+const fname = document.getElementById('fname');
+const sname = document.getElementById('sname');
+
+//onsubmmit -->
+
+function welcome(argument) {
+	// body...
+
+	document.getElementById('form').style.display = "none";
+
+	document.getElementById('h1').innerHTML = "My Dashboard";
+
+	document.getElementById("demo").innerHTML = `welcome ${lname} ${sname} to your new home`;
+}
